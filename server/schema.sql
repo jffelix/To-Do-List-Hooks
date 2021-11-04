c0622fae52cab417523953c2f4d0d0a16b2f82a5
Jean-Luc Felix <jlfelix522@yahoo.com>
2021-11-04 13:59:13 -0700

    Created table in schema.sql

--- server/schema.sql
+++ server/schema.sql
@@ -4,3 +4,14 @@ CREATE DATABASE todoHooks;
 
 USE todoHooks;
 
+CREATE TABLE shoppingList {
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    quantity INT NOT NULL,
+    PRIMARY KEY (id)
+};
+
+INSERT INTO shoppingList (name, quantity) VALUES ("water", 12.45);
+INSERT INTO shoppingList (name, quantity) VALUES ("office chair", 24.99);
+INSERT INTO shoppingList (name, quantity) VALUES ("gift card", 50.00);
+INSERT INTO shoppingList (name, quantity) VALUES ("candy", 1.99);
