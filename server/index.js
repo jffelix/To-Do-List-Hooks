f7c87ce2d500768ed55915abf4936e1bc76d7327
Jean-Luc Felix <jlfelix522@yahoo.com>
2021-11-05 16:17:57 -0700

    Refactored server/index.js to import routers

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 
+const router = require('./routers/index.js');
 const path = require('path');
 const app = express();
 const port = 3001;
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '..', '/client/dist')));
+
+app.use('/', router);
 
 app.listen(port, () => {
     console.log(`Listening from port ${port}!`);
