a7dc8b422ea1a73dfdefc76431534c1cf0ccf2bf
Jean-Luc Felix <jlfelix522@yahoo.com>
2021-11-05 16:11:46 -0700

    Created getItems in controllers/index.js

---
+++ server/controllers/index.js
@@ -0,0 +1,17 @@
+const dbHelpers = require('../models/index.js');
+
+const controllers = {
+    getItems: function(req, res) {
+        dbHelpers.getItems((err, results) => {
+            if (err) {
+                res.status(404).send(err);
+                console.log('Error received at getItems in controllers.');
+            } else {
+                res.status(200).send(results);
+                console.log('Successfully connected from getItems in controllers!');
+            }
+        })
+    }
+}
+
+module.exports = controllers;
