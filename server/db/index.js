fabce78bef1587b414c961fe8230a331b7ce0c36
Jean-Luc Felix <jlfelix522@yahoo.com>
2021-11-05 15:15:14 -0700

    Created progress on server/db/index.js

---
+++ server/db/index.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "todoHooks"
+});
+
+db.connect((err, results) => {
+    if (err) {
+        console.log("Error received at db: ", err);
+    } else {
+        console.log("Successfully connected at db!");
+    }
+})
+
+module.exports = db;
