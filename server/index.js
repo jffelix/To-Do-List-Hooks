c17c97445b4f34cd57a63319f68c8062023befa8
Jean-Luc Felix <jlfelix522@yahoo.com>
2021-11-16 22:53:51 -0800

    Changed src pathfile in index.html

--- server/index.js
+++ server/index.js
@@ -4,11 +4,11 @@ const cors = require('cors');
 const router = require('./routers/index.js');
 const path = require('path');
 const app = express();
-const port = 3001;
+const port = 3000;
 
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, '..', '/client/public')));
+app.use(express.static(path.join(__dirname, '../client/public')));
 
 app.use('/', router);
 
