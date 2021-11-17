04fe9d245e5b281a0a1057c4d66ea3f7b484d579
Jean-Luc Felix <jlfelix522@yahoo.com>
2021-11-17 00:29:49 -0800

    Added progress to ItemListEntry.jsx

---
+++ client/src/sub-components/ItemListEntry.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function ItemListEntry() {
+
+    return (
+        <div>
+            <h4>Hello from ItemListEntry!</h4>
+        </div>
+    )
+
+}
+
+export default ItemListEntry;
