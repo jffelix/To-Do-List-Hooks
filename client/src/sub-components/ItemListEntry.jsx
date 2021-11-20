6ccca9ddb754dcf74498a98476af41aa9eb5a3b5
Jean-Luc Felix <jlfelix522@yahoo.com>
2021-11-20 14:20:22 -0800

    Began work on UpdateItem in ItemListEntry.jsx

--- client/src/sub-components/ItemListEntry.jsx
+++ client/src/sub-components/ItemListEntry.jsx
@@ -6,6 +6,11 @@ import axios from 'axios';
 function ItemListEntry(props) {
     // const contextListEntry = useContext(ItemList.contextList);
 
+    function UpdateItem(event) {
+        var selectedItemId = props.item.id;
+        console.log('selectedItemId: ', selectedItemId);
+    }
+
     function DeleteItem(event) {
         var selectedItemId = props.item.id;
 
@@ -23,7 +28,7 @@ function ItemListEntry(props) {
         <div>
             <p>{props.item.name}</p>
             <p>{props.item.quantity}</p>
-            <button>Update Item</button>
+            <button onClick={(event) => UpdateItem(event)}>Update Item</button>
             <button onClick={(event) => DeleteItem(event)}>Delete Item</button>
             <p>________________________________________</p>
         </div>
