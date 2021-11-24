983b097b20d873c34c687ce81e332898ed2ae5a1
Jean-Luc Felix <jlfelix522@yahoo.com>
2021-11-23 17:05:35 -0800

    Began work on testing GET request

---
+++ server/spec/httpHandler.spec.js
@@ -0,0 +1,14 @@
+// const { iteratee } = require('lodash');
+// const { describe } = require('yargs');
+const expect = require('chai').expect();
+const app = express();
+// const router = require('../routers/index.js');
+const appComponent = require('../client/src/App.jsx');
+
+describe('CRUD operations', () => {
+
+    it('should receive a response from a GET request', (done) => {
+        
+    })
+
+})
