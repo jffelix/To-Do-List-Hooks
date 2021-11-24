c7425fb5f7567919e680c0a7b202213f4102ec4f
Jean-Luc Felix <jlfelix522@yahoo.com>
2021-11-24 14:44:38 -0800

    Began additional progress on spec folder

--- server/spec/httpHandler.spec.js
+++ server/spec/httpHandler.spec.js
@@ -8,7 +8,11 @@ const appComponent = require('../client/src/App.jsx');
 describe('CRUD operations', () => {
 
     it('should receive a response from a GET request', (done) => {
-        
-    })
+        app.get('/items')
+        .end((err, res) => {
+            expect(res).to.have.status(200);
+            done();
+        })
+    });
 
-})
+});
